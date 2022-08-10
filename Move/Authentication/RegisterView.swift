ad288cb133c2a4053c7802f51f458b5389744a42
daria-andrioaie <daria.andrioaie@gmail.com>
2022-08-10 13:06:11 +0300

    added icons with actions to fields

--- Move/Authentication/RegisterView.swift
+++ Move/Authentication/RegisterView.swift
@@ -7,49 +7,6 @@
 
 import SwiftUI
 
-extension View {
-    func placeholder<Content: View>(when shouldShow: Bool, @ViewBuilder placeholder: () -> Content) -> some View {
-        ZStack(alignment: .leading) {
-            placeholder().opacity(shouldShow ? 1 : 0)
-            self
-        }
-    }
-}
-
-struct UnderlinedTextField: View {
-    var placeholder: String
-    @Binding var binding: String
-    var isPrivate: Bool
-    
-    var body: some View {
-        if isPrivate {
-            SecureField("", text: $binding)
-                .frame(width: 337)
-    //                    .offset(x: -5)
-                .placeholder(when: binding.isEmpty) {
-                    Text(placeholder).foregroundColor(Color("NeutralPurple"))
-                        .font(.custom("BaiJamjuree-Medium", size: 16))
-                }
-                
-                .overlay(Rectangle().frame(height: 1).padding(.top, 45))
-                .foregroundColor(Color("NeutralPurple"))
-                .padding(.bottom, 50)
-        }
-        else {
-            TextField("", text: $binding)
-                .frame(width: 337)
-    //                    .offset(x: -5)
-                .placeholder(when: binding.isEmpty) {
-                    Text(placeholder).foregroundColor(Color("NeutralPurple"))
-                        .font(.custom("BaiJamjuree-Medium", size: 16))
-                }
-                .overlay(Rectangle().frame(height: 1).padding(.top, 45))
-                .foregroundColor(Color("NeutralPurple"))
-                .padding(.bottom, 50)
-        }
-    }
-}
-
 struct RegisterView: View {
     @State private var emailAddress = ""
     @State private var username = ""
@@ -78,11 +35,11 @@ struct RegisterView: View {
                     .padding(.bottom, 24)
      
                 
-                UnderlinedTextField(placeholder: "Email address", binding: $emailAddress, isPrivate: false)
+                SimpleUnderlinedTextField(placeholder: "Email address", binding: $emailAddress)
                 
-                UnderlinedTextField(placeholder: "Username", binding: $username, isPrivate: false)
+                SimpleUnderlinedTextField(placeholder: "Username", binding: $username)
 
-                UnderlinedTextField(placeholder: "Password", binding: $password, isPrivate: true)
+                SecureUnderlinedTextField(placeholder: "Password", binding: $password)
                 
                 Text("By continuing you agree to Move’s  Terms and Conditions and Privacy Policy")
                     .font(.custom("BaiJamjuree-Regular", size: 12))
---
+++ Move/Utils/SimpleUnderlinedTextField.swift
@@ -0,0 +1,130 @@
+//
+//  UnderlinedTextField.swift
+//  Move
+//
+//  Created by Daria Andrioaie on 10.08.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    func placeholder<Content: View>(when shouldShow: Bool, @ViewBuilder placeholder: () -> Content) -> some View {
+        ZStack(alignment: .leading) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
+}
+
+struct SecureUnderlinedTextField: View {
+    var placeholder: String
+    
+    @Binding var binding: String
+    @FocusState var fieldIsFocused: Bool
+    @State private var showText = false
+    
+    var body: some View {
+        ZStack {
+            if !showText {
+                SecureField("", text: $binding)
+                    .frame(width: 337)
+                    .placeholder(when: binding.isEmpty) {
+                        Text(placeholder).foregroundColor(.white)
+                            .opacity(0.6)
+                            .font(.custom("BaiJamjuree-Medium", size: 16))
+                    }
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+                    .focused($fieldIsFocused)
+                    .overlay(Rectangle().frame(height: fieldIsFocused ? 2 : 1).padding(.top, 45))
+                    .foregroundColor(.white)
+                    .font(.custom("BaiJamjuree-Medium", size: 16))
+                    .opacity(fieldIsFocused ? 1 : 0.6)
+                    .padding(.bottom, 50)
+                
+                if fieldIsFocused {
+                    Button {
+                        print("button pressed")
+                        showText.toggle()
+                    } label: {
+                        Image("eye-opened")
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .padding(.trailing, 55)
+                            .offset(y: -20)
+                    }
+                }
+            }
+            else {
+                TextField("", text: $binding)
+                    .frame(width: 337)
+                    .placeholder(when: binding.isEmpty) {
+                        Text(placeholder).foregroundColor(.white)
+                            .opacity(0.6)
+                            .font(.custom("BaiJamjuree-Medium", size: 16))
+                    }
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+                    .focused($fieldIsFocused)
+                    .overlay(Rectangle().frame(height: fieldIsFocused ? 2 : 1).padding(.top, 45))
+                    .foregroundColor(.white)
+                    .font(.custom("BaiJamjuree-Medium", size: 16))
+                    .opacity(fieldIsFocused ? 1 : 0.6)
+                    .padding(.bottom, 50)
+                
+                if fieldIsFocused {
+                    Button {
+                        print("button pressed")
+                        showText.toggle()
+                    } label: {
+                        Image("eye-closed")
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .padding(.trailing, 55)
+                            .offset(y: -20)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct SimpleUnderlinedTextField: View {
+    var placeholder: String
+    
+    @Binding var binding: String
+    @FocusState var fieldIsFocused: Bool
+    
+    var body: some View {
+        ZStack {
+            TextField("", text: $binding)
+                .frame(width: 337)
+                .placeholder(when: binding.isEmpty) {
+                    Text(placeholder).foregroundColor(.white)
+                        .opacity(0.6)
+                        .font(.custom("BaiJamjuree-Medium", size: 16))
+                }
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+                .focused($fieldIsFocused)
+                .overlay(Rectangle().frame(height: fieldIsFocused ? 2 : 1).padding(.top, 45))
+                .foregroundColor(.white)
+                .font(.custom("BaiJamjuree-Medium", size: 16))
+                .opacity(fieldIsFocused ? 1 : 0.6)
+                .padding(.bottom, 50)
+            if fieldIsFocused {
+                Button {
+                    binding = ""
+                } label: {
+                    Image("close-circle")
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .padding(.trailing, 45)
+                        .offset(y: -20)
+                }
+            }
+        }
+            
+    }
+}
